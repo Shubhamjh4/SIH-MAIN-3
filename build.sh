b4bd933b01411e8cdd7f558ed67edb3209db33bd
Shubham Kumar Jha <153086776+Shubhamjh4@users.noreply.github.com>
2025-09-05 17:10:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,5 +10,9 @@ python manage.py collectstatic --no-input
 # apply database migrations
 python manage.py migrate
 
-# create superuser if not exists
+# create superuser (set your username, email, and password here)
+export DJANGO_SUPERUSER_USERNAME=rajan
+export DJANGO_SUPERUSER_EMAIL=rajan@123
+# export DJANGO_SUPERUSER_PASSWORD=yourpassword   # 👈 must be set
+
 python manage.py createsuperuser --noinput || true
